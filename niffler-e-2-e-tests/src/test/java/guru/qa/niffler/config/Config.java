174f7fbabca3b9ebfc2bcfa02e451d5fb6d5764d
AGordey <gordey.andrej@gmail.com>
2026-01-08 11:35:24 +0300

    Video 7.1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/Config.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/Config.java
@@ -6,8 +6,11 @@ public interface Config {
 
     @Nonnull
     static Config getInstance() {
-        return LocalConfig.INSTANCE;
+        return ("docker".equals(System.getProperty("test.env")))
+                ? DockerConfig.INSTANCE
+                : LocalConfig.INSTANCE;
     }
+
     @Nonnull
     String frontUrl();
 
