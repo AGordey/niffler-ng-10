1a73f5d73267b7f63a1ecea398df168cd62e138c
Agordey <gordey.andrej@gmail.com>
2026-01-18 11:23:23 +0300

    Video_9.1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
@@ -1,6 +1,7 @@
 package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
@@ -8,10 +9,17 @@ import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.component.Header;
+import guru.qa.niffler.util.ScreenDiffResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$;
 import static guru.qa.niffler.util.RandomDataUtils.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @WebTest
 public class SpendingTest {
@@ -57,5 +65,25 @@ public class SpendingTest {
                 .save()
                 .checkSnackbarText("New spending is successfully created")
                 .checkThatTableContains(description);
+    }
+
+
+    @User(
+            spendings = @Spending(
+                    category = "Обучение",
+                    description = "Обучение Advanced 2.0",
+                    amount = 79990
+            )
+    )
+    @ScreenShotTest("img/expected-stat.png")
+    void checkStatComponentTest(UserJson user, BufferedImage expected) throws IOException {
+        Selenide.open(LoginPage.URL, LoginPage.class)
+                .login(user.username(), user.testData().password());
+
+        BufferedImage actual = ImageIO.read($("canvas[role='img']").screenshot());
+        assertFalse(new ScreenDiffResult(
+                expected,
+                actual
+        ));
     }
 }
